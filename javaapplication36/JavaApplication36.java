5bbadb85145bf678fa4bb693bb41672954781b21
YOUR NAME <YOUR@EMAIL.com>
2016-09-13 12:58:22 +0530

    String rotation

---
+++ javaapplication36/JavaApplication36.java
@@ -0,0 +1,57 @@
+
+package javaapplication36;
+
+/**
+ *
+ * @author sachin
+ */
+import java.util.*;
+public class JavaApplication36 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int f=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st String");
+        String s1 = sc.next();
+        System.out.println("Enter the 2nd String");
+        String s2 = sc.next();
+        int i=0,j=0;
+        if(s1.length()!=s2.length()){
+            System.out.println("Both Sting is not rotation");
+            return;
+        }
+        while(i<s1.length()){
+            if(s1.charAt(i)!=s2.charAt(j)){
+                j++;
+            }
+            if(j>s1.length()-1){
+                System.out.println("Both Sting is not rotation");
+            return;
+            }
+           else{
+               
+               for(i=0;i<s1.length();i++){
+                   f=1;
+                   if(s1.charAt(i)==s2.charAt(j)){
+                       j++;
+                       if(j>s1.length()-1)
+                           j= j-s1.length();
+                       }
+                   else{f=0;
+                       System.out.println("Both Sting is not rotation");
+                         return;
+                   }
+               }
+                               
+            }
+        }
+        if(f==1)
+            System.out.println("Both Sting is in rotation");
+            
+    }
+    
+}
